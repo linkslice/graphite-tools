0760a2e7964923673b3b5923356d121694982bcd
Bryan Irvine <sparctacus@gmail.com>
2016-10-14 11:48:16 -0700

    Create pickledump.py
    When using debug mode on a lot of the scripts in this repo it creates a data.p file. This script makes it easier to read those files, by opening and converting the output into graphite line format for easier troubleshooting.

---
+++ pickledump.py
@@ -0,0 +1,29 @@
+#!/bin/env python
+
+
+from optparse import OptionParser
+import sys
+import os
+import pickle
+
+
+
+
+
+def main():
+    parser = OptionParser()
+    parser.add_option('-f', '--file', dest='filename',
+        default='data.p',
+        help='location of data.p or other pickle.')
+    options, args = parser.parse_args()
+
+
+    data = pickle.load(open(options.filename))
+
+    i = 0
+    while i < len(data):
+        print ("%s %s %s") % (data[i][0], data[i][1][1], data[i][1][0])
+        i = i + 1 
+
+if __name__ == '__main__':
+    main()
